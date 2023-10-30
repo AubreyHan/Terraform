adc1aa7f75477b6b79be3f0258099bef519853fa
Aubrey Han <aubreyhan@live.com>
2023-10-30 03:25:16 +0000

    GCP init

---
+++ GCP/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "/home/gcpvm/ServiceAccount/hy-20231030-001-e55877804b4e.json"
+  project = "hy-20231030-001"
+}
